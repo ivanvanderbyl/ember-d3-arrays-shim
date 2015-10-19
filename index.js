df4486cd32d2a211afc85dd01519c6cd21014ef3
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-10-19 17:29:45 +0200

    Mods

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
   name: 'Ember D3 Arrays Shim',
 
-  init: function(name) {
-    var assets_path = require('path').join('d3-arrays','src','index.js');
-    this.treePaths.vendor = require.resolve('d3-arrays').replace(assets_path, '');
-  },
+  // init: function(name) {
+  //   var assets_path = require('path').join('d3-arrays','src','index.js');
+  //   this.treePaths.vendor = require.resolve('d3-arrays').replace(assets_path, '');
+  // },
 
   included: function(app) {
-    console.log('d3-arrays included', app);
+    // console.log('d3-arrays included', app);
+
+    app.import(path.join(app.project.nodeModulesPath, 'd3-arrays', 'src', 'index.js'));
 
     // var options = this.app.options.icAjaxOptions || {enabled: true};
 
