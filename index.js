309090d8d616bcc0376a3d4c6a496ee08efab162
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-10-20 17:11:02 +0200

    Shims

--- index.js
+++ index.js
@@ -2,27 +2,28 @@
 'use strict';
 
 var d3ArraysFactory = require('d3-arrays');
+export * from d3ArraysFactory;
 
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.returnExports = factory();
-  }
-}(this, function () {
+// (function (root, factory) {
+//     if (typeof define === 'function' && define.amd) {
+//         // AMD. Register as an anonymous module.
+//         define([], factory);
+//     } else if (typeof module === 'object' && module.exports) {
+//         // Node. Does not work with strict CommonJS, but
+//         // only CommonJS-like environments that support module.exports,
+//         // like Node.
+//         module.exports = factory();
+//     } else {
+//         // Browser globals (root is window)
+//         root.returnExports = factory();
+//   }
+// }(this, function () {
 
-    // Just return a value to define the module export.
-    // This example returns an object, but the module
-    // can return a function as the exported value.
-    return d3ArraysFactory;
-}));
+//     // Just return a value to define the module export.
+//     // This example returns an object, but the module
+//     // can return a function as the exported value.
+//     return d3ArraysFactory;
+// }));
 
 // var path = require('path');
 
