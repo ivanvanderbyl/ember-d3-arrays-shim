40c00eca30eaced9ad1f3eec3f09e905674eeccf
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2015-10-20 17:23:49 +0200

    Shims

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
 /* jshint node: true */
 'use strict';
 
-// var d3ArraysFactory = require('d3-arrays');
-// export * from d3ArraysFactory;
+var d3ArraysFactory = require('d3-arrays');
 
-// (function (root, factory) {
-//     if (typeof define === 'function' && define.amd) {
-//         // AMD. Register as an anonymous module.
-//         define([], factory);
-//     } else if (typeof module === 'object' && module.exports) {
-//         // Node. Does not work with strict CommonJS, but
-//         // only CommonJS-like environments that support module.exports,
-//         // like Node.
-//         module.exports = factory();
-//     } else {
-//         // Browser globals (root is window)
-//         root.returnExports = factory();
-//   }
-// }(this, function () {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
 
-//     // Just return a value to define the module export.
-//     // This example returns an object, but the module
-//     // can return a function as the exported value.
-//     return d3ArraysFactory;
-// }));
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return d3ArraysFactory;
+}));
 
 // var path = require('path');
 
-module.exports = {
-  name: 'ember-d3-arrays-shim',
+// module.exports = {
+//   name: 'ember-d3-arrays-shim',
 
-//   init: function(name) {
-//     var assets_path = require('path').join('d3-arrays','index.js');
-//     this.treePaths.vendor = require.resolve('d3-arrays').replace(assets_path, '');
-//   },
+// //   init: function(name) {
+// //     var assets_path = require('path').join('d3-arrays','index.js');
+// //     this.treePaths.vendor = require.resolve('d3-arrays').replace(assets_path, '');
+// //   },
 
-//   included: function(app) {
-//     // console.log('d3-arrays included', app);
+// //   included: function(app) {
+// //     // console.log('d3-arrays included', app);
 
-//     // this.app.import(path.join(this.app.project.nodeModulesPath, 'd3-arrays', 'src', 'index.js'));
+// //     // this.app.import(path.join(this.app.project.nodeModulesPath, 'd3-arrays', 'src', 'index.js'));
 
-//     // var options = this.app.options.icAjaxOptions || {enabled: true};
+// //     // var options = this.app.options.icAjaxOptions || {enabled: true};
 
-//     // if (options.enabled) {
-//     //   this.app.import('vendor/ic-ajax/dist/named-amd/main.js', {
-//     //     exports: {
-//     //       'ic-ajax': [
-//     //         'default',
-//     //         'defineFixture',
-//     //         'lookupFixture',
-//     //         'raw',
-//     //         'request',
-//     //       ]
-//     //     }
-//     //   });
-//     // }
-//   }
-};
+// //     // if (options.enabled) {
+// //     //   this.app.import('vendor/ic-ajax/dist/named-amd/main.js', {
+// //     //     exports: {
+// //     //       'ic-ajax': [
+// //     //         'default',
+// //     //         'defineFixture',
+// //     //         'lookupFixture',
+// //     //         'raw',
+// //     //         'request',
+// //     //       ]
+// //     //     }
+// //     //   });
+// //     // }
+// //   }
+// };
